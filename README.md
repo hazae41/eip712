420077e05d41bd25923a53a164dee8bea7342d4c
hazae41 <4405263+hazae41@users.noreply.github.com>
2026-06-10 13:10:39 +0700

    c

--- README.md
+++ README.md
@@ -23,38 +23,38 @@ npm install @hazae41/eip712
 const hash: Uint8Array = eip712.hash({
   types: {
     EIP712Domain: [
-      { name: 'name', type: 'string' },
-      { name: 'version', type: 'string' },
-      { name: 'chainId', type: 'uint256' },
-      { name: 'verifyingContract', type: 'address' },
+      { name: "name", type: "string" },
+      { name: "version", type: "string" },
+      { name: "chainId", type: "uint256" },
+      { name: "verifyingContract", type: "address" },
     ],
     Person: [
-      { name: 'name', type: 'string' },
-      { name: 'wallet', type: 'address' }
+      { name: "name", type: "string" },
+      { name: "wallet", type: "address" }
     ],
     Mail: [
-      { name: 'from', type: 'Person' },
-      { name: 'to', type: 'Person' },
-      { name: 'contents', type: 'string' }
+      { name: "from", type: "Person" },
+      { name: "to", type: "Person" },
+      { name: "contents", type: "string" }
     ],
   },
-  primaryType: 'Mail',
+  primaryType: "Mail",
   domain: {
-    name: 'Ether Mail',
-    version: '1',
+    name: "Ether Mail",
+    version: "1",
     chainId: 1n,
-    verifyingContract: '0xCcCCccccCCCCcCCCCCCcCcCccCcCCCcCcccccccC',
+    verifyingContract: "0xCcCCccccCCCCcCCCCCCcCcCccCcCCCcCcccccccC",
   },
   message: {
     from: {
-      name: 'Cow',
-      wallet: '0xCD2a3d9F938E13CD947Ec05AbC7FE734Df8DD826',
+      name: "Cow",
+      wallet: "0xCD2a3d9F938E13CD947Ec05AbC7FE734Df8DD826",
     },
     to: {
-      name: 'Bob',
-      wallet: '0xbBbBBBBbbBBBbbbBbbBbbbbBBbBbbbbBbBbbBBbB',
+      name: "Bob",
+      wallet: "0xbBbBBBBbbBBBbbbBbbBbbbbBBbBbbbbBbBbbBBbB",
     },
-    contents: 'Hello, Bob!',
+    contents: "Hello, Bob!",
   },
 })
 ```
--- src/mods/eip712/mod.test.ts
+++ src/mods/eip712/mod.test.ts
@@ -5,38 +5,38 @@ test("eip712 #1", () => {
   const hash = eip712.hash({
     types: {
       EIP712Domain: [
-        { name: 'name', type: 'string' },
-        { name: 'version', type: 'string' },
-        { name: 'chainId', type: 'uint256' },
-        { name: 'verifyingContract', type: 'address' },
+        { name: "name", type: "string" },
+        { name: "version", type: "string" },
+        { name: "chainId", type: "uint256" },
+        { name: "verifyingContract", type: "address" },
       ],
       Person: [
-        { name: 'name', type: 'string' },
-        { name: 'wallet', type: 'address' }
+        { name: "name", type: "string" },
+        { name: "wallet", type: "address" }
       ],
       Mail: [
-        { name: 'from', type: 'Person' },
-        { name: 'to', type: 'Person' },
-        { name: 'contents', type: 'string' }
+        { name: "from", type: "Person" },
+        { name: "to", type: "Person" },
+        { name: "contents", type: "string" }
       ],
     },
-    primaryType: 'Mail',
+    primaryType: "Mail",
     domain: {
-      name: 'Ether Mail',
-      version: '1',
+      name: "Ether Mail",
+      version: "1",
       chainId: 1n,
-      verifyingContract: '0xCcCCccccCCCCcCCCCCCcCcCccCcCCCcCcccccccC',
+      verifyingContract: "0xCcCCccccCCCCcCCCCCCcCcCccCcCCCcCcccccccC",
     },
     message: {
       from: {
-        name: 'Cow',
-        wallet: '0xCD2a3d9F938E13CD947Ec05AbC7FE734Df8DD826',
+        name: "Cow",
+        wallet: "0xCD2a3d9F938E13CD947Ec05AbC7FE734Df8DD826",
       },
       to: {
-        name: 'Bob',
-        wallet: '0xbBbBBBBbbBBBbbbBbbBbbbbBBbBbbbbBbBbbBBbB',
+        name: "Bob",
+        wallet: "0xbBbBBBBbbBBBbbbBbbBbbbbBBbBbbbbBbBbbBBbB",
       },
-      contents: 'Hello, Bob!',
+      contents: "Hello, Bob!",
     },
   })
 
@@ -45,34 +45,34 @@ test("eip712 #1", () => {
 
 test("eip712 #2", () => {
   const hash = eip712.hash({
-    primaryType: 'Mail',
+    primaryType: "Mail",
     domain: {
-      name: 'Ether Mail',
-      version: '1',
+      name: "Ether Mail",
+      version: "1",
       chainId: 1,
-      verifyingContract: '0x0000000000000000000000000000000000000000',
+      verifyingContract: "0x0000000000000000000000000000000000000000",
     },
     types: {
       Person: [
-        { name: 'name', type: 'string' },
-        { name: 'wallet', type: 'address' },
+        { name: "name", type: "string" },
+        { name: "wallet", type: "address" },
       ],
       Mail: [
-        { name: 'from', type: 'Person' },
-        { name: 'to', type: 'Person' },
-        { name: 'contents', type: 'string' },
+        { name: "from", type: "Person" },
+        { name: "to", type: "Person" },
+        { name: "contents", type: "string" },
       ],
     },
     message: {
       from: {
-        name: 'Cow',
-        wallet: '0xCD2a3d9F938E13CD947Ec05AbC7FE734Df8DD826',
+        name: "Cow",
+        wallet: "0xCD2a3d9F938E13CD947Ec05AbC7FE734Df8DD826",
       },
       to: {
-        name: 'Bob',
-        wallet: '0xbBbBBBBbbBBBbbbBbbBbbbbBBbBbbbbBbBbbBBbB',
+        name: "Bob",
+        wallet: "0xbBbBBBBbbBBBbbbBbbBbbbbBBbBbbbbBbBbbBBbB",
       },
-      contents: 'Hello, Bob!',
+      contents: "Hello, Bob!",
     },
   })
 
@@ -81,55 +81,55 @@ test("eip712 #2", () => {
 
 test("eip712 #3", () => {
   const hash = eip712.hash({
-    primaryType: 'Mail',
+    primaryType: "Mail",
     domain: {
-      name: 'Ether Mail 🥵',
-      version: '1.1.1',
+      name: "Ether Mail 🥵",
+      version: "1.1.1",
       chainId: 1,
-      verifyingContract: '0x0000000000000000000000000000000000000000',
+      verifyingContract: "0x0000000000000000000000000000000000000000",
     },
     types: {
       Name: [
-        { name: 'first', type: 'string' },
-        { name: 'last', type: 'string' },
+        { name: "first", type: "string" },
+        { name: "last", type: "string" },
       ],
       Person: [
-        { name: 'name', type: 'Name' },
-        { name: 'wallet', type: 'address' },
-        { name: 'favoriteColors', type: 'string[3]' },
-        { name: 'foo', type: 'uint256' },
-        { name: 'age', type: 'uint8' },
-        { name: 'isCool', type: 'bool' },
+        { name: "name", type: "Name" },
+        { name: "wallet", type: "address" },
+        { name: "favoriteColors", type: "string[3]" },
+        { name: "foo", type: "uint256" },
+        { name: "age", type: "uint8" },
+        { name: "isCool", type: "bool" },
       ],
       Mail: [
-        { name: 'timestamp', type: 'uint256' },
-        { name: 'from', type: 'Person' },
-        { name: 'to', type: 'Person' },
-        { name: 'contents', type: 'string' },
-        { name: 'hash', type: 'bytes' },
+        { name: "timestamp", type: "uint256" },
+        { name: "from", type: "Person" },
+        { name: "to", type: "Person" },
+        { name: "contents", type: "string" },
+        { name: "hash", type: "bytes" },
       ],
     },
     message: {
       timestamp: 1234567890n,
-      contents: 'Hello, Bob! 🖤',
-      hash: '0x1234567890abcdef1234567890abcdef1234567890abcdef1234567890abcdef',
+      contents: "Hello, Bob! 🖤",
+      hash: "0x1234567890abcdef1234567890abcdef1234567890abcdef1234567890abcdef",
       from: {
         name: {
-          first: 'Cow',
-          last: 'Burns',
+          first: "Cow",
+          last: "Burns",
         },
-        wallet: '0xCD2a3d9F938E13CD947Ec05AbC7FE734Df8DD826',
+        wallet: "0xCD2a3d9F938E13CD947Ec05AbC7FE734Df8DD826",
         age: 69,
         foo: 123123123123123123n,
-        favoriteColors: ['red', 'green', 'blue'],
+        favoriteColors: ["red", "green", "blue"],
         isCool: false,
       },
       to: {
-        name: { first: 'Bob', last: 'Builder' },
-        wallet: '0xbBbBBBBbbBBBbbbBbbBbbbbBBbBbbbbBbBbbBBbB',
+        name: { first: "Bob", last: "Builder" },
+        wallet: "0xbBbBBBBbbBBBbbbBbbBbbbbBBbBbbbbBbBbbBBbB",
         age: 70,
         foo: 123123123123123123n,
-        favoriteColors: ['orange', 'yellow', 'green'],
+        favoriteColors: ["orange", "yellow", "green"],
         isCool: true,
       },
     },
